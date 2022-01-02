68e99319194c50e56983f3c115ecd54c99003ac1
Gordon Lei <gl1776@nyu.edu>
2022-01-01 20:36:42 -0500

    preliminary files to start working on GraphQL and MongoDB Atlas integration

---
+++ src/routes/keyboards.jsx
@@ -0,0 +1,9 @@
+function Keyboards() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Keyboards</h2>
+    </main>
+  );
+}
+
+export default Keyboards;
---
+++ src/routes/keysets.jsx
@@ -0,0 +1,9 @@
+function KeySets() {
+  return (
+    <main style={{ padding: "1rem 0" }}>
+      <h2>KeySets</h2>
+    </main>
+  );
+}
+
+export default KeySets;
